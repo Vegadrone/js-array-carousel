df33dbc259a297e93f236fe6f95b7332466b2c7e
Vegadrone <spenaantonino@gmail.com>
2022-06-20 15:50:32 +0200

    Update: milestone 1, static image and buttons

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,48 @@
-console.log ('ciao')
+//!Consegna:
+// * Dato un array contenente una lista di sei immagini(url), creare un carosello come nello screenshot allegato.
+// *     MILESTONE 1
+// * Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro:
+// * avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+// *     MILESTONE 2
+// * Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito
+// * e un semplice ciclo for che concatena un template literal.
+// * Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+// * Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1,
+// * ma costruito dinamicamente attraverso JavaScript.
+// *     MILESTONE 3
+// * Al click dell'utente sui bottoni/frecce, il programma cambierà l’immagine attiva,
+// * che quindi verrà visualizzata al posto della precedente.
+// * BONUS 1:
+// * Aggiungere il ciclo infinito del carosello.Ovvero se è attiva la prima immagine e l'utente clicca la freccia
+// * per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
+// * BONUS 2:
+// * Aggiungere la visualizzazione thumbnails, realizzate a piacere(pallini o miniature).
+// * Tutte le thumbnails avranno una classe inattiva, tranne quella corrispondente all’immagine attiva,
+// * che invece avrà una classe attiva(che la distinguerà con un bordo, un colore, ecc).
+// * Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
+// TODO Prima di partire a scrivere codice:
+// TODO Non lasciamoci spaventare dalla complessità apparente dell'esercizio, ma analizziamo prima,
+// TODO come abbiamo fatto sempre, cosa ci potrebbe aspettare.
+// TODO Abbiamo completato ormai da qualche giorno la sessione HTML e CSS,
+// TODO se non ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti.
+// TODO Non dedichiamo però al ripasso più di una mezz'ora, così da non perdere di vista il focus dell'esercizio.
+// TODO Consigli del giorno:
+// TODO Costruiamo del carosello una versione statica contenente solamente un'immagine. Di questa versione
+// TODO statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js.
+// TODO Potremo quindi usarli come "template".
+// TODO Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
+// TODO Al momento giusto(ihihhi starà a voi capire quale) rispondete a questa domanda: "Quanti cicli servono?"
+
+
+ 
+const images = [
+    'https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg',
+    'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg',
+    'https://cdn.photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg',
+    'https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg',
+    'https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg',
+    'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'
+];
+
+console.log(images);
+
